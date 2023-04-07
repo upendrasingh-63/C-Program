b110dcf9fae2d7ba9b05a854bc197e64ec7913c9
upendrasingh-63 <126307145+upendrasingh-63@users.noreply.github.com>
2023-04-07 09:08:05 +0530

    Create right rotate.c

---
+++ Lab c ques/Array/right rotate.c
@@ -0,0 +1,26 @@
+#include <stdio.h>    
+     
+int main()    
+{    
+    int arr[] = {1, 2, 3, 4, 5};     
+    int length = sizeof(arr)/sizeof(arr[0]);    
+    int n = 2,i,j;    
+    printf("Original array: \n");    
+    for (i = 0; i < length; i++) {     
+        printf("%d ", arr[i]);     
+    }      
+	for(i = 0; i < n; i++){    
+        int j, last;    
+        last = arr[length-1];    
+        for(j = length-1; j >0; j--){    
+            arr[j] = arr[j-1];    
+        }    
+        arr[0] = last;    
+    }    
+    printf("\n");      
+    printf("Array after right rotation: \n");    
+    for(i = 0; i < length; i++){    
+        printf("%d ", arr[i]);    
+    }    
+    return 0;    
+}    
