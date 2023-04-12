fd45c3d794d0be3d8702e5cdafb4b5915627bddc
upendrasingh-63 <126307145+upendrasingh-63@users.noreply.github.com>
2023-04-12 17:48:55 +0530

    Create matrix multiplication.c

---
+++ Lab c ques/Array/matrix multiplication.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int a[2][2]={{1,2,},{3,4}},b[2][2]={{1,2,},{3,4}},c[2][2],i,j,k;
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			c[i][j]=0;
+			for(k=0;k<2;k++)
+			{
+				c[i][j]+=a[i][k]*b[k][j];
+			}
+		}
+	}
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			printf("element [%d][%d] %d\n",i,j,c[i][j]);
+		}
+	}
+	return 0;
+}
