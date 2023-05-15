ca027b6032ae60dccf984bb9d7600ddfbab33864
upendrasingh-63 <126307145+upendrasingh-63@users.noreply.github.com>
2023-05-15 12:05:37 +0530

    Create StringCmp.c

---
+++ Lab c ques/String/StringCmp.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    char str1[100], str2[100];
+    int i = 0, flag = 0;
+
+    printf("Enter the first string: ");
+    gets(str1);
+
+    printf("Enter the second string: ");
+    gets(str2);
+
+    while (str1[i] != '\0' || str2[i] != '\0') {
+        if (str1[i] != str2[i]) {
+            flag = 1;
+            break;
+        }
+        i++;
+    }
+
+    if (flag == 0 && str1[i] == '\0' && str2[i] == '\0') {
+        printf("Strings are equal\n");
+    } else {
+        printf("Strings are not equal\n");
+    }
+
+    return 0;
+}
