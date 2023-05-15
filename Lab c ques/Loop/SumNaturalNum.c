9f46e568c66b031e8154a3b2be65779051c8aac9
upendrasingh-63 <126307145+upendrasingh-63@users.noreply.github.com>
2023-05-16 00:18:04 +0530

    Create SumNaturalNum.c

---
+++ Lab c ques/Loop/SumNaturalNum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0, i = 1;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    while (i <= n) {
+        sum += i;
+        i++;
+    }
+
+    printf("Sum of natural numbers from 1 to %d is %d\n", n, sum);
+
+    return 0;
+}
