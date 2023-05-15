d06290b9709b4fb40cfd935761c76fa8be4cb993
upendrasingh-63 <126307145+upendrasingh-63@users.noreply.github.com>
2023-05-15 23:25:16 +0530

    Create VowelConsonant.c

---
+++ Lab c ques/Switch Case/VowelConsonant.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    char alphabet;
+
+    printf("Enter an alphabet: ");
+    scanf(" %c", &alphabet);
+
+    switch (alphabet) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+            printf("%c is a vowel.\n", alphabet);
+            break;
+        default:
+            printf("%c is a consonant.\n", alphabet);
+            break;
+    }
+
+    return 0;
+}
