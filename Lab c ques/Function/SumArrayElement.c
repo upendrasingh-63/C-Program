937b27bccbb80b1350ee7da546cd76130c762b0f
upendrasingh-63 <126307145+upendrasingh-63@users.noreply.github.com>
2023-05-15 21:29:40 +0530

    Create SumArrayElement.c

---
+++ Lab c ques/Function/SumArrayElement.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int sumOfArray(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int main() {
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    int arr[size];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int sum = sumOfArray(arr, size);
+    printf("Sum of array elements is %d\n", sum);
+    return 0;
+}
