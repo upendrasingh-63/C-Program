7758a10cc0e5d6b89d1bf4fb5e76c4a070ff0f62
upendrasingh-63 <126307145+upendrasingh-63@users.noreply.github.com>
2023-05-15 15:13:29 +0530

    Update UpperToLower.c

--- Lab c ques/String/UpperToLower.c
+++ Lab c ques/String/UpperToLower.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 
-int main() {
-    char str[100];
-    int i = 0;
+#include <ctype.h>
 
-    printf("Enter a string: ");
+#include <string.h>
+
+int main() {
+
+    char str[100];
+
+    printf("Enter a string in uppercase: ");
+
     gets(str);
 
-    while (str[i] != '\0') {
-        if (str[i] >= 'A' && str[i] <= 'Z') {
-            str[i] = str[i] + 32;
-        }
-        i++;
+    int length = strlen(str);
+
+    int i;
+
+    for (i = 0; i < length; i++) {
+
+        str[i] = tolower(str[i]);
+
     }
 
-    printf("Lowercase
+    printf("String in lowercase: %s\n", str);
+
+    return 0;
+
+}
