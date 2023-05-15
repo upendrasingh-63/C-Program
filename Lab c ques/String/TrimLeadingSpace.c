9584cb92367f4c309eba5958a1cdf7f9411db091
upendrasingh-63 <126307145+upendrasingh-63@users.noreply.github.com>
2023-05-15 15:24:30 +0530

    Create TrimLeadingSpace.c

---
+++ Lab c ques/String/TrimLeadingSpace.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#include <string.h>
+
+int main() {
+
+    char str[100];
+
+    int i, j, length;
+
+    printf("Enter a string: ");
+
+    gets(str);
+
+    length = strlen(str);
+
+    for (i = 0; i < length; i++) {
+
+        if (str[i] != ' ' && str[i] != '\t') {
+
+            break;
+
+        }
+
+    }
+
+    for (j = 0; i < length; i++, j++) {
+
+        str[j] = str[i];
+
+    }
+
+    str[j] = '\0';
+
+    printf("String after trimming leading white space characters: %s\n", str);
+
+    return 0;
+
+}
+
