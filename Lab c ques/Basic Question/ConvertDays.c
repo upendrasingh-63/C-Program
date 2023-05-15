ec0f1f0fbe9d59871d84f3b2a5932efdbba183e4
upendrasingh-63 <126307145+upendrasingh-63@users.noreply.github.com>
2023-05-15 23:43:01 +0530

    Create ConvertDays.c

---
+++ Lab c ques/Basic Question/ConvertDays.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int days, years, weeks, remainingDays;
+
+    printf("Enter the number of days: ");
+    scanf("%d", &days);
+
+    years = days / 365;
+    weeks = (days % 365) / 7;
+    remainingDays = (days % 365) % 7;
+
+    printf("Years: %d\n", years);
+    printf("Weeks: %d\n", weeks);
+    printf("Days: %d\n", remainingDays);
+
+    return 0;
+}
