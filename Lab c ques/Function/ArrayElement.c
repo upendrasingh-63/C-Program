034519daeadf0b979dcd9b4f24d33be6e7757e8f
upendrasingh-63 <126307145+upendrasingh-63@users.noreply.github.com>
2023-05-15 21:29:13 +0530

    Create ArrayElement.c

---
+++ Lab c ques/Function/ArrayElement.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void displayArray(int arr[], int size) {
+    printf("Array elements: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    int arr[size];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+    displayArray(arr, size);
+    return 0;
+}
