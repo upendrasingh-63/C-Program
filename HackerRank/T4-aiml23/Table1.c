0a6c7cfb8c204e3a10b139021a2fdb60f3604bd1
upendrasingh-63 <126307145+upendrasingh-63@users.noreply.github.com>
2023-05-16 08:43:06 +0530

    Create Table1.c

---
+++ HackerRank/T4-aiml23/Table1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a,i=1;
+    scanf("%d",&a);
+    while(i<=10)
+    {
+        printf("%d\n",a*i);
+        i++;
+    }
+    return 0;
+}
