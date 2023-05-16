4f99d6a2400240ece5024b7895de976986389637
upendrasingh-63 <126307145+upendrasingh-63@users.noreply.github.com>
2023-05-16 08:26:29 +0530

    Create ProductOf2Number.c

---
+++ HackerRank/T1-aiml23/ProductOf2Number.c
@@ -0,0 +1,15 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main() {
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printf("%d",a*b);
+    return 0;
+}
