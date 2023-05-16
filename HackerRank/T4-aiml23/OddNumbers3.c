3e3e25e963cfc4e7a3ba7acdacbfff750ead0ebf
upendrasingh-63 <126307145+upendrasingh-63@users.noreply.github.com>
2023-05-16 08:49:51 +0530

    Create OddNumbers3.c

---
+++ HackerRank/T4-aiml23/OddNumbers3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+   int a,i=1;
+    scanf("%d",&a);
+    while(i<=a)
+    {
+        printf("%d ",i);
+        i+=2;
+    }
+    return 0;
+}
